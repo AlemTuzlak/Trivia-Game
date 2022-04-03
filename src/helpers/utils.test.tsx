3cca3576fc84168cf8bc61ae14973488d2b3217f
mistery15 <89130300+mistery15@users.noreply.github.com>
2022-04-03 02:17:09 +0200

    Added lint fix

--- src/helpers/utils.test.tsx
+++ src/helpers/utils.test.tsx
@@ -1,8 +1,12 @@
-import { decodeHtml } from './decodeHtml';
+import { decodeHtml } from "./decodeHtml";
 
-describe('util tests', () => {
-  it('decodes html strings properly', () => {
-    const string = decodeHtml('The first &quot;Metal Gear&quot; game was released for the PlayStation 1.');
-    expect(string).toBe('The first \"Metal Gear\" game was released for the PlayStation 1.')
-  })
-})
+describe("util tests", () => {
+  it("decodes html strings properly", () => {
+    const string = decodeHtml(
+      "The first &quot;Metal Gear&quot; game was released for the PlayStation 1."
+    );
+    expect(string).toBe(
+      'The first "Metal Gear" game was released for the PlayStation 1.'
+    );
+  });
+});
